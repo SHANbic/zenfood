f85e979a051e07909b9b46afb032a88cbebefad1
SHANbic <supagoof924@hotmail.com>
2019-10-06 18:26:45 +0200

    [ CONFIG ] Handled error in http requests

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,15 +1,33 @@
 import React, { useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar.js';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'paris'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (e) {
+      setErrorMessage("Couldn't fetch your datas");
+    }
+  };
 
   return (
     <View>
-      <Text>Search Screen</Text>
-      <SearchBar term={term} onTermChange={setTerm}
-      onTermSubmit={()=>console.log('Term was submitted')} />
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <Text>found {results.length} results</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
     </View>
   );
 };
