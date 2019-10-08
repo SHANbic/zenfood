96c17b2de2bc01b91ed73a5e43979c2d2268ca22
SHANbic <supagoof924@hotmail.com>
2019-10-08 21:59:11 +0200

    [ FEATURE ] Now displaying list of restaurants names

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -5,7 +5,6 @@ export default () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
   const searchApi = async searchTerm => {
-    console.log('#');
     try {
       const response = await yelp.get('/search', {
         params: {
