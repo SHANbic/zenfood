eeb26443f3f102f3661f5634c1e6ff3716c6266b
SHANbic <supagoof924@hotmail.com>
2019-10-08 21:04:00 +0200

    [ REFACTO ] Created a custom hook to separate logic concern in the app

---
+++ src/hooks/useResults.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import yelp from '../api/yelp';
+
+export default () => {
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const searchApi = async searchTerm => {
+    console.log('#');
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: 'paris'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (e) {
+      setErrorMessage("Couldn't fetch your datas");
+    }
+  };
+
+  useEffect(() => {
+    searchApi('sushi');
+  }, []);
+
+  return [searchApi, results, errorMessage];
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,32 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar.js';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const searchApi = async searchTerm => {
-    console.log('#');
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'paris'
-        }
-      });
-      setResults(response.data.businesses);
-    } catch (e) {
-      setErrorMessage("Couldn't fetch your datas");
-    }
-  };
-
-  useEffect(() => {
-    searchApi('sushi');
-  }, []);
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View>
