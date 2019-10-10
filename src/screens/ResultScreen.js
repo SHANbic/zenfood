8a303ac3ad92aaf4579351f36acc318750ce6fe8
SHANbic <supagoof924@hotmail.com>
2019-10-10 22:15:13 +0200

    [ FEATURE ] Handled data fetching for displaying a single restaurant detailed informations

--- src/screens/ResultScreen.js
+++ src/screens/ResultScreen.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import yelp from '../api/yelp';
 
 const ResultScreen = ({ navigation }) => {
   const id = navigation.getParam('id');
+
+  const [result, setResult] = useState(null);
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  const getResult = async id => {
+    const response = await yelp.get(`/${id}`);
+    console.log(response.data);
+    setResult(response.data);
+  };
   return (
     <View>
       <Text>result show screen</Text>
