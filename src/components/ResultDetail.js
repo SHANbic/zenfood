0b3585cdcc6f5a1e57fa6d2fadf74e80ade15ce5
SHANbic <supagoof924@hotmail.com>
2019-10-10 20:16:11 +0200

    [ DESIGN ] Now displaying restaurants name and image

--- src/components/ResultDetail.js
+++ src/components/ResultDetail.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const ResultDetail = ({ result }) => {
   return (
     <View>
-      <Text>{result.name}</Text>
+      <Image style={styles.image} source={{ uri: result.image_url }} />
+      <Text style={styles.text}>{result.name}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 240,
+    height: 120,
+    marginHorizontal: 5,
+    borderRadius: 3
+  },
+  text: {
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+});
 
 export default ResultDetail;
